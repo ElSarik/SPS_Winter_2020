0df9744ae45534b7067ef63b49522b7cb60fc0d3
ElSarik <sarikakisilias@gmail.com>
2020-12-03 17:57:14 +0200

    Lesson 3_12_2020 working
    Finished Task 308, functional code

--- src/java/managedBeans/ProductMB.java
+++ src/java/managedBeans/ProductMB.java
@@ -2,21 +2,21 @@ package managedBeans;
 
 import EJB_session.ProductSB;
 import JPA_entities.Product;
+import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+//import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 
 @Named(value = "productMB")
-@RequestScoped
-public class ProductMB 
+@SessionScoped
+public class ProductMB implements Serializable
 {
     @EJB
     private ProductSB productSB;
     List<Product> products;
     private Product product = new Product() ;
-    
-    
     
     public ProductMB()
     {
@@ -34,16 +34,23 @@ public class ProductMB
         products = productSB.findProducts();
     }
     
-    public void createProduct()
+    public String prepareCreateProduct()
+    {
+        product = new Product();
+        return "createUpdateProduct";
+    }
+    
+    public String createProduct()
     {
        productSB.persistProduct(getProduct()); //ISOS product ANTI GIA getProduct()
+       return "productCatalog";
     }
 
     public Product getProduct() {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
+//    public void setProduct(Product product) {
+//        this.product = product;
+//    }
 }
